9769b28a069ee848c687fa83fee4dfe90c5841d5
galing <galinglauuu@gmail.com>
2025-11-10 08:41:09 +0800

    feat:load chain info add across deposit address

--- loader/src_tx.go
+++ loader/src_tx.go
@@ -76,11 +76,13 @@ func (mgr *SrcTxManager) Save(tx *SrcTx) error {
 	tx.TargetAddress.String = strings.TrimSpace(tx.TargetAddress.String)
 	tx.SrcTokenName.String = strings.TrimSpace(tx.SrcTokenName.String)
 
-	query := `INSERT IGNORE INTO t_src_transaction (chainid, tx_hash, sender, receiver, target_address, token, value, dst_chainid, is_testnet, tx_timestamp, src_token_name, src_token_decimal, is_cctp, src_nonce, thirdparty_channel, to_exchange)
+	query := `INSERT IGNORE INTO t_src_transaction (chainid, tx_hash, sender, receiver, target_address, token, value, dst_chainid, is_testnet, tx_timestamp, src_token_name, src_token_decimal, is_cctp, src_nonce,
+       thirdparty_channel, to_exchange, is_across, across_status, across_tx_data)
               VALUES (?, ?, ?, ?, ?, ?, ? , ?, ?, ?, ?, ?, ?, ?, ?, ?)`
 
-	// Execute the SQL statement with tx data
-	_, err := mgr.db.Exec(query, tx.ChainId, tx.TxHash, tx.Sender, tx.Receiver, tx.TargetAddress, tx.Token, tx.Value, tx.DstChainid, tx.IsTestnet, tx.TxTimestamp, tx.SrcTokenName, tx.SrcTokenDecimal, tx.IsCctp, tx.SrcNonce, tx.ThirdpartyChannel, tx.ToExchange)
+	// Execute the SQL statement with tx datas
+	_, err := mgr.db.Exec(query, tx.ChainId, tx.TxHash, tx.Sender, tx.Receiver, tx.TargetAddress, tx.Token, tx.Value, tx.DstChainid, tx.IsTestnet, tx.TxTimestamp, tx.SrcTokenName, tx.SrcTokenDecimal, tx.IsCctp, tx.SrcNonce,
+		tx.ThirdpartyChannel, tx.ToExchange, tx.IsAcross, tx.AcrossStatus, tx.AcrossTxData)
 	if err != nil {
 		mgr.alerter.AlertText("failed to insert src transaction", err)
 		return err
